e58ace3aff47ebbc73dfbb9f1a158ea86f65f259
Vorakan S <vorakan.sumethsenee@wdc.com>
2026-06-08 11:09:18 +0700

    Add tick library and delayed rectangle drawing
    Introduce tick.lua (rxi's timer library) and classic.lua (OOP base), integrate tick into the main loop to delay drawing a rectangle by 2 seconds, randomize new rectangle spawn positions, and add a run.bat launcher.

--- main.lua
+++ main.lua
@@ -1,16 +1,21 @@
 -- can be use only this file
-local bigrect_state = require("bigrect")
+local bigrect_state = require "bigrect"
 if bigrect_state then
   print(bigrect_state)
 end
 
 function love.load()
   -- init global to use in muliple files
+  tick = require "tick"
   listOfRectangles = {}
+  drawRectangles = false
+  -- delay 2 secion than draw rect
+  tick.delay(function () drawRectangles = true end, 2)
 end
 
 function love.update(dt)
   -- update pos
+  tick.update(dt)
   for i,v in ipairs(listOfRectangles) do
     v.x = v.x + v.s * dt
   end
@@ -20,6 +25,9 @@ function love.draw()
   -- draw graphic
   for i,v in ipairs(listOfRectangles) do
     love.graphics.rectangle("fill", v.x, v.y, v.w, v.h)
+  end
+  if drawRectangles then
+    love.graphics.rectangle("fill", 400,400,300,200)
   end
 end
 
@@ -35,8 +43,8 @@ end
 
 function createRect()
   rect={}
-  rect.x=100
-  rect.y=100
+  rect.x=math.random(100,500)
+  rect.y=math.random(100,500)
   rect.w=100
   rect.h=100
   rect.s=100
